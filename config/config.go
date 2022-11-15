5719a711e15f044d3c934e39970a6bd70fea98ef
Thanathip <dannajaeiei@hotmail.com>
2022-11-15 18:24:51 +0700

    feat: Added mysql database config variables

--- config/config.go
+++ config/config.go
@@ -1,25 +1,31 @@
 package config
 
 import (
-	"os"
-
-	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
 )
 
 type Config struct {
-	App App
+	Env            string `mapstructure:"ENV"`
+	GinMode        string `mapstructure:"GIN_MODE" default:"release"`
+	ServerAddress  string `mapstructure:"SERVER_ADDRESS"`
+	MYSQL_HOSTNAME string `mapstructure:"MYSQL_HOSTNAME"`
+	MYSQL_PORT     string `mapstructure:"MYSQL_PORT"`
+	MYSQL_USERNAME string `mapstructure:"MYSQL_USERNAME"`
+	MYSQL_PASSWORD string `mapstructure:"MYSQL_PASSWORD"`
+	MYSQL_DATABASE string `mapstructure:"MYSQL_DATABASE"`
 }
 
-type App struct {
-	Port string `mapstructure:"PORT"`
-}
+func LoadConfig(path string) (config *Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(".env")
 
-func LoadConfig() *Config {
-	_ = godotenv.Load(".env")
-	var appConfig App
-	appConfig.Port = os.Getenv("PORT")
+	viper.AutomaticEnv()
 
-	return &Config{
-		App: appConfig,
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
 	}
+
+	err = viper.Unmarshal(&config)
+	return
 }
