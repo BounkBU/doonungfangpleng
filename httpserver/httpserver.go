300b8aa92f6291c7bd084a3672d7e1d009300e38
Thanathip <dannajaeiei@hotmail.com>
2022-11-15 19:11:34 +0700

    fix: Server type and add database to server

--- httpserver/httpserver.go
+++ httpserver/httpserver.go
@@ -4,30 +4,33 @@ import (
 	"github.com/BounkBU/doonungfangpleng/config"
 	"github.com/BounkBU/doonungfangpleng/handler"
 	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
 )
 
 type Server struct {
-	Gin    *gin.Engine
-	Config *config.Config
+	App      *gin.Engine
+	Database *sqlx.DB
+	Config   *config.Config
 }
 
-func NewHTTPServer(config *config.Config) *Server {
-	gin.SetMode(config.GinMode)
+func NewHTTPServer(config *config.Config, db *sqlx.DB) *Server {
+	gin.SetMode(config.App.GinMode)
 	app := gin.Default()
 	return &Server{
-		Gin:    app,
-		Config: config,
+		App:      app,
+		Database: db,
+		Config:   config,
 	}
 }
 
 func (server *Server) SetupRouter() {
 	healthcheckHandler := handler.NewHealthcheckHandler()
 
-	server.Gin.GET("/", healthcheckHandler.GetServerInformation)
-	server.Gin.GET("/health", healthcheckHandler.GetHealthcheck)
+	server.App.GET("/", healthcheckHandler.GetServerInformation)
+	server.App.GET("/health", healthcheckHandler.GetHealthcheck)
 }
 
 func (server *Server) Start() {
 	server.SetupRouter()
-	server.Gin.Run(server.Config.ServerAddress)
+	server.App.Run(server.Config.App.ServerAddress)
 }
