2754c5c1b75bce0f73362d164cc18021a472c9a5
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-11-17 00:34:56 +0700

    fix: Type of id and created_at

--- pkg/database/migration/000001_init_schema.up.sql
+++ pkg/database/migration/000001_init_schema.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `search_movies` (
-  `id` bigserial PRIMARY KEY AUTO_INCREMENT,
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
   `tmdb_movie_id` varchar(255),
   `title` varchar(255),
   `overview` varchar(255),
@@ -8,16 +8,17 @@ CREATE TABLE `search_movies` (
   `release_date` varchar(255),
   `rating` float,
   `search_amount` bigint NOT NULL,
-  `created_at` timestamptz NOT NULL DEFAULT (now())
+  `created_at` TIMESTAMP NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE `playlists` (
-  `id` bigserial PRIMARY KEY AUTO_INCREMENT,
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
   `movie_id` varchar(255),
   `spotify_playlist_id` varchar(255),
   `name` varchar(255),
   `owner` varchar(255),
   `image_path` varchar(255),
   `visit_amount` bigint NOT NULL DEFAULT 1,
-  `created_at` timestamptz NOT NULL DEFAULT (now())
+  `created_at` TIMESTAMP NOT NULL DEFAULT (now())
 );
+
