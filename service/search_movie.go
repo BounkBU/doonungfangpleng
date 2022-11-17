d9235986c2e988dd4b52cb60be81f2a1b812c578
Thanathip <dannajaeiei@hotmail.com>
2022-11-17 17:31:49 +0700

    feat: Create or update search movie service

---
+++ service/search_movie.go
@@ -0,0 +1,42 @@
+package service
+
+import (
+	"database/sql"
+	"errors"
+
+	log "github.com/sirupsen/logrus"
+
+	model "github.com/BounkBU/doonungfangpleng/models"
+	"github.com/BounkBU/doonungfangpleng/repository"
+)
+
+type SearchMovieService struct {
+	searchMovieRepository *repository.SearchMovieRepository
+}
+
+type SearchMovieServiceInterface interface {
+	CreateOrUpdateSearchMovie(newSearchMovie model.CreateSearchMovieRequest) error
+}
+
+func NewSearchMovieSearchService(searchMovieRepository *repository.SearchMovieRepository) *SearchMovieService {
+	return &SearchMovieService{
+		searchMovieRepository: searchMovieRepository,
+	}
+}
+
+func (s *SearchMovieService) CreateOrUpdateSearchMovie(newSearchMovie model.CreateSearchMovieRequest) error {
+	log.Info("Start creating or updating new search movie")
+	defer log.Info("End creating or updating new search movie")
+	searchMovie, err := s.searchMovieRepository.SelectSearchMovieByTMDBMovieId(newSearchMovie.TMDBMovieId)
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		return err
+	}
+
+	if errors.Is(err, sql.ErrNoRows) {
+		err = s.searchMovieRepository.InsertSearchMovie(newSearchMovie)
+		return err
+	}
+
+	err = s.searchMovieRepository.UpdateSearchMovieSearchAmount(searchMovie.TMDBMovieId)
+	return err
+}
