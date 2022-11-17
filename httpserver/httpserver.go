13c9e3ffafa3d895415739e9f235d496c3bb70a9
Thanathip <dannajaeiei@hotmail.com>
2022-11-17 17:37:18 +0700

    feat: Added search movie handler to server

--- httpserver/httpserver.go
+++ httpserver/httpserver.go
@@ -4,6 +4,8 @@ import (
 	"github.com/BounkBU/doonungfangpleng/config"
 	_ "github.com/BounkBU/doonungfangpleng/docs"
 	"github.com/BounkBU/doonungfangpleng/handler"
+	"github.com/BounkBU/doonungfangpleng/repository"
+	"github.com/BounkBU/doonungfangpleng/service"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
@@ -43,13 +45,16 @@ func NewHTTPServer(config *config.Config, db *sqlx.DB) *Server {
 func (server *Server) SetupRouter() {
 	// Enable cors request
 	server.App.Use(cors.Default())
-
 	server.App.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	healthcheckHandler := handler.NewHealthcheckHandler()
-
 	server.App.GET("/", healthcheckHandler.GetServerInformation)
 	server.App.GET("/health", healthcheckHandler.GetHealthcheck)
+
+	searchMovieRepository := repository.NewSearchMovieRepository(server.Database)
+	searchMovieService := service.NewSearchMovieSearchService(searchMovieRepository)
+	searchMovieHandler := handler.NewSearchMovieHandler(searchMovieService)
+	server.App.POST("/movies", searchMovieHandler.CreateSearchMovieHandler)
 }
 
 func (server *Server) Start() {
