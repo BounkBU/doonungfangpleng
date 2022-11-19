63132a63bad24e910872bef68f43abe67ff2b39f
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-11-20 01:47:28 +0700

    feat: Added model tmdb movie detail

--- models/tmdb.go
+++ models/tmdb.go
@@ -1,18 +1,41 @@
 package model
 
-type AllTmdbMovieResult struct {
-	Page         int64       `json:"page"`
-	Results      []TmdbMovie `json:"results"`
-	TotalPages   int64       `json:"total_pages"`
-	TotalResults int64       `json:"total_results"`
+type PopularTmdbMovie struct {
+	Page         int64                      `json:"page"`
+	Results      []PopularTmdbMovieResponse `json:"results"`
+	TotalPages   int64                      `json:"total_pages"`
+	TotalResults int64                      `json:"total_results"`
 }
 
-type TmdbMovie struct {
+type PopularTmdbMovieResponse struct {
 	ID         int64  `json:"id"`
 	PosterPath string `json:"poster_path"`
 	Title      string `json:"title"`
 }
 
-type GetAllPopularMovieFromTmdbRequest struct {
-	Page int `json:"page"`
+type TmdbMovieDetail struct {
+	Genres       []Genre `json:"genres"`
+	ID           int64   `json:"id"`
+	Overview     string  `json:"overview"`
+	BackdropPath string  `json:"backdrop_path"`
+	PosterPath   string  `json:"poster_path"`
+	ReleaseDate  string  `json:"release_date"`
+	Title        string  `json:"title"`
+	VoteAverage  float64 `json:"vote_average"`
+}
+
+type TmdbMovieDetailResponse struct {
+	ID           int64   `json:"tmdb_id"`
+	Title        string  `json:"title"`
+	BackdropPath string  `json:"backdrop_path"`
+	Genres       string  `json:"genres"`
+	Overview     string  `json:"overview"`
+	ImagePath    string  `json:"image_path"`
+	ReleaseDate  string  `json:"release_date"`
+	Rating       float64 `json:"rating"`
+}
+
+type Genre struct {
+	ID   int64  `json:"id"`
+	Name string `json:"name"`
 }
