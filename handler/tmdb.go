40e208c9830e8ace062d768fb7e28462530f23b8
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-11-20 01:49:31 +0700

    feat: Added get tmdb movie detail handler

--- handler/tmdb.go
+++ handler/tmdb.go
@@ -2,34 +2,50 @@ package handler
 
 import (
 	"net/http"
+	"strconv"
 
-	model "github.com/BounkBU/doonungfangpleng/models"
 	"github.com/BounkBU/doonungfangpleng/service"
 	"github.com/gin-gonic/gin"
 )
 
 type tmdbHandler struct {
-	TmdbService service.TmdbService
+	tmdbService service.TmdbService
 }
 
 func NewTmdbHandler(tmdbService service.TmdbService) tmdbHandler {
 	return tmdbHandler{
-		TmdbService: tmdbService,
+		tmdbService: tmdbService,
 	}
 }
 
 func (h *tmdbHandler) GetAllPopularMovieFromTmdbHandler(c *gin.Context) {
-	var req model.GetAllPopularMovieFromTmdbRequest
-
-	if err := c.BindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, errorResponse(ErrInvalidRequestData))
+	page, err := strconv.Atoi(c.DefaultQuery("page", "1"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, errorResponse(ErrInvalidQueryParam))
 		return
 	}
 
-	data, err := h.TmdbService.GetPopularMovieFromTmdb(1)
+	tmdbMovies, err := h.tmdbService.GetPopularMovieFromTmdb(page)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
 	}
 
-	c.JSON(http.StatusOK, data)
+	c.JSON(http.StatusOK, tmdbMovies)
+}
+
+func (h *tmdbHandler) GetMovieDetailHandler(c *gin.Context) {
+	movieId, err := strconv.Atoi(c.Param("movieId"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, errorResponse(ErrInvalidQueryParam))
+		return
+	}
+
+	tmdbMovie, err := h.tmdbService.GetMovieDetailFromTmdb(movieId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	c.JSON(http.StatusOK, tmdbMovie)
 }
