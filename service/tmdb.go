9c339da3a6780fc62d45e682c51acb2c80943caa
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-11-20 01:49:10 +0700

    feat: Added get movie detail from tmdb service

--- service/tmdb.go
+++ service/tmdb.go
@@ -2,31 +2,33 @@ package service
 
 import (
 	"fmt"
+	"strings"
 
 	model "github.com/BounkBU/doonungfangpleng/models"
 	"github.com/BounkBU/doonungfangpleng/repository"
 )
 
 type tmdbService struct {
-	TmdbRepository repository.TmdbRepository
+	tmdbRepository repository.TmdbRepository
 }
 
 type TmdbService interface {
-	GetPopularMovieFromTmdb(page int) ([]model.TmdbMovie, error)
+	GetPopularMovieFromTmdb(page int) ([]model.PopularTmdbMovieResponse, error)
+	GetMovieDetailFromTmdb(movieId int) (model.TmdbMovieDetailResponse, error)
 }
 
 func NewTmdbService(tmdbRepository repository.TmdbRepository) *tmdbService {
 	return &tmdbService{
-		TmdbRepository: tmdbRepository,
+		tmdbRepository: tmdbRepository,
 	}
 }
 
-func (t *tmdbService) GetPopularMovieFromTmdb(page int) ([]model.TmdbMovie, error) {
-	movies, err := t.TmdbRepository.QueryPopularMovies(page)
+func (t *tmdbService) GetPopularMovieFromTmdb(page int) ([]model.PopularTmdbMovieResponse, error) {
+	movies, err := t.tmdbRepository.QueryPopularMovies(page)
 
-	tmdbMovies := []model.TmdbMovie{}
+	tmdbMovies := []model.PopularTmdbMovieResponse{}
 	for _, movie := range movies {
-		tmdbMovie := model.TmdbMovie{
+		tmdbMovie := model.PopularTmdbMovieResponse{
 			ID:         movie.ID,
 			Title:      movie.Title,
 			PosterPath: fmt.Sprintf("https://image.tmdb.org/t/p/w300_and_h450_bestv2%s", movie.PosterPath),
@@ -35,4 +37,27 @@ func (t *tmdbService) GetPopularMovieFromTmdb(page int) ([]model.TmdbMovie, erro
 	}
 
 	return tmdbMovies, err
+}
+
+func (t *tmdbService) GetMovieDetailFromTmdb(movieId int) (model.TmdbMovieDetailResponse, error) {
+	movie, err := t.tmdbRepository.QueryMovieDetails(movieId)
+
+	var genres []string
+
+	for _, genre := range movie.Genres {
+		genres = append(genres, genre.Name)
+	}
+
+	tmdbMovie := model.TmdbMovieDetailResponse{
+		ID:           movie.ID,
+		Overview:     movie.Overview,
+		Genres:       strings.Join(genres, ", "),
+		BackdropPath: fmt.Sprintf("https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces%s", movie.BackdropPath),
+		ImagePath:    fmt.Sprintf("https://image.tmdb.org/t/p/w300_and_h450_bestv2%s", movie.PosterPath),
+		ReleaseDate:  movie.ReleaseDate,
+		Title:        movie.Title,
+		Rating:       movie.VoteAverage,
+	}
+
+	return tmdbMovie, err
 }
