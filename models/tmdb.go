9ceecea44a70cf6ae41da9b415a8226069c7bb3f
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-11-20 17:33:50 +0700

    feat: Added tmdb serie and serie detail model

--- models/tmdb.go
+++ models/tmdb.go
@@ -1,19 +1,13 @@
 package model
 
 type PopularTmdbMovie struct {
-	Page         int64                      `json:"page"`
-	Results      []PopularTmdbMovieResponse `json:"results"`
-	TotalPages   int64                      `json:"total_pages"`
-	TotalResults int64                      `json:"total_results"`
+	Page         int64                 `json:"page"`
+	Results      []PopularTmdbResponse `json:"results"`
+	TotalPages   int64                 `json:"total_pages"`
+	TotalResults int64                 `json:"total_results"`
 }
 
-type PopularTmdbMovieResponse struct {
-	ID         int64  `json:"id"`
-	PosterPath string `json:"poster_path"`
-	Title      string `json:"title"`
-}
-
-type TmdbMovieDetail struct {
+type TmdbMovieDetailRequest struct {
 	Genres       []Genre `json:"genres"`
 	ID           int64   `json:"id"`
 	Overview     string  `json:"overview"`
@@ -35,7 +29,57 @@ type TmdbMovieDetailResponse struct {
 	Rating       float64 `json:"rating"`
 }
 
+type PopularTmdbSerie struct {
+	Page         int64                     `json:"page"`
+	Results      []PopularTmdbSerieRequest `json:"results"`
+	TotalPages   int64                     `json:"total_pages"`
+	TotalResults int64                     `json:"total_results"`
+}
+
+type PopularTmdbSerieRequest struct {
+	ID         int64  `json:"id"`
+	Name       string `json:"name"`
+	PosterPath string `json:"poster_path"`
+}
+
+type PopularTmdbResponse struct {
+	ID         int64  `json:"id"`
+	Title      string `json:"title"`
+	PosterPath string `json:"poster_path"`
+}
+
+type TmdbSerieDetailRequest struct {
+	ID              int64    `json:"id"`
+	BackdropPath    string   `json:"backdrop_path"`
+	Genres          []Genre  `json:"genres"`
+	Name            string   `json:"name"`
+	NumberOfSeasons int64    `json:"number_of_seasons"`
+	Overview        string   `json:"overview"`
+	PosterPath      string   `json:"poster_path"`
+	Seasons         []Season `json:"seasons"`
+	VoteAverage     float64  `json:"vote_average"`
+}
+
+type TmdbSerieDetailResponse struct {
+	ID              int64    `json:"tmdb_id"`
+	Title           string   `json:"title"`
+	BackdropPath    string   `json:"backdrop_path"`
+	Genres          string   `json:"genres"`
+	Overview        string   `json:"overview"`
+	ImagePath       string   `json:"image_path"`
+	NumberOfSeasons int64    `json:"number_of_seasons"`
+	Seasons         []Season `json:"seasons"`
+	Rating          float64  `json:"rating"`
+}
+
 type Genre struct {
 	ID   int64  `json:"id"`
 	Name string `json:"name"`
+}
+
+type Season struct {
+	AirDate      string `json:"air_date"`
+	Name         string `json:"name"`
+	EpisodeCount int64  `json:"episode_count"`
+	PosterPath   string `json:"poster_path"`
 }
